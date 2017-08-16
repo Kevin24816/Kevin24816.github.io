bb5061537e59b47eeb0d099d2f88feeaa33e39c6
kevin <qkl1@rice.edu>
2017-08-15 19:58:59 -0500

    Added notes for other sections

--- script.js
+++ script.js
@@ -8,15 +8,30 @@ $( function() {
 
 // TODO: CREATE FUNCTION TO ADJUST CONTENT WIDTHS BASED OFF BROWSER SIZE
 // https://stackoverflow.com/questions/8717506/dynamically-resize-div-based-on-size-of-browser-window
-// var width = $(window).width();
-// window.alert("window width: " + width);
-// original_width = $(".content").width().get();
-// if (width > 750) {
-//     $(".content").width(.70 * width);
-// } else {
-//     $(".content").width(original_width);
-// }
 
+var adjustContentWidth = function() {
+    var width = $(window).width();
+    widthChange = 1500;
+
+    if (width > widthChange) {
+        $(".width-adapter").css( "padding", "0 200px" )
+    } else {
+        $(".width-adapter").css("padding", "0");
+    }
+};
+
+var addEvent = function(object, type, callback) {
+    if (object === null || typeof(object) === 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+
+addEvent(window, "resize", adjustContentWidth);
 
 // TODO: ANIMATE THE TERMINAL WINDOW USING WEBKIT
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup
